222eecbf68f3b8f157d161c98183df092b24718e
Vansh Chaurasiya <vanshchaurasiya1557@gmail.com>
2024-11-08 23:04:19 +0530

    Add config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'placehold.jp/**',
+              port: '**',
+              pathname: '**',
+            },
+          ],
+          formats: ['image/webp', 'image/avif'],
+      },
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,12 +73,30 @@ const config: Config = {
     				to: {
     					height: '0'
     				}
+    			},
+    			marquee: {
+    				from: {
+    					transform: 'translateX(0)'
+    				},
+    				to: {
+    					transform: 'translateX(calc(-100% - var(--gap)))'
+    				}
+    			},
+    			'marquee-vertical': {
+    				from: {
+    					transform: 'translateY(0)'
+    				},
+    				to: {
+    					transform: 'translateY(calc(-100% - var(--gap)))'
+    				}
     			}
     		},
     		animation: {
     			'accordion-down': 'accordion-down 0.2s ease-out',
-    			'accordion-up': 'accordion-up 0.2s ease-out'
-    		},
+    			'accordion-up': 'accordion-up 0.2s ease-out',
+    			marquee: 'marquee var(--duration) infinite linear',
+    			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite'
+    		}
     	},
     	plugins: [tailwindcssAnimate]
     },
