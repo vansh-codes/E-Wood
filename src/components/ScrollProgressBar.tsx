7d8ab5a74317939f39f63a758c2a1e7b78f5844c
Vansh Chaurasiya <vanshchaurasiya1557@gmail.com>
2024-11-19 19:50:11 +0530

    color contrast fix

--- src/components/ScrollProgressBar.tsx
+++ src/components/ScrollProgressBar.tsx
@@ -20,7 +20,7 @@ export function ScrollProgressBar() {
   return (
     <div className="fixed top-0 left-0 w-full h-1 bg-background z-50">
       <div
-        className="h-full bg-[#DE8F5F] dark:bg-[#A66E38] transition-all duration-300 ease-out"
+        className="h-full bg-[#27272a] dark:bg-[#e4e4e7] transition-all duration-300 ease-out"
         style={{ width: `${scrollProgress}%` }}
       />
     </div>
